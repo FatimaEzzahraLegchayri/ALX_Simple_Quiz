6286c16eea054e8b7bb96217c8104c0a28aedd21
Fatima Ezzahra Legchayri <fatimaezzahra.legchayri@gmail.com>
2024-10-03 15:12:59 +0200

    update

--- quiz.js
+++ quiz.js
@@ -1,14 +1,15 @@
 const submit = document.getElementById('submit-answer')
 const feedback = document.getElementById('feedback')
 
-function checkAnswer(choice1, choice2, choice3){
+function checkAnswer(){
     let correctAnswer = "4";   
-    const userAnswer = document.querySelector('input[name="quiz"]:checked')
+    const userAnswer = document.querySelector(`input[name = 'quiz']:checked`)  
     
-    if(correctAnswer === userAnswer){
+    if(correctAnswer === userAnswer["value"]){
         feedback.textContent = 'Correct! Well done.'
     }else{
         feedback.textContent = "That's incorrect. Try again!"
     }
 }
 submit.addEventListener('click',checkAnswer)
+console.log('user ',userAnswer);
