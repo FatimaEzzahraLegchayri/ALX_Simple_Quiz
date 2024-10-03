d1471a8ea4a87e88862d8c38d7e510539049a944
Fatima Ezzahra Legchayri <fatimaezzahra.legchayri@gmail.com>
2024-10-03 14:12:07 +0200

    update

--- quiz.js
+++ quiz.js
@@ -4,9 +4,8 @@ const feedback = document.getElementById('feedback')
 function checkAnswer(choice1, choice2, choice3){
     let correctAnswer = "4";   
     const userAnswer = document.querySelector('input[name="quiz"]:checked')
-    console.log('nvnvn',userAnswer);
     
-    if(correctAnswer == userAnswer.value){
+    if(correctAnswer === userAnswer){
         feedback.textContent = 'Correct! Well done.'
     }else{
         feedback.textContent = "That's incorrect. Try again!"
